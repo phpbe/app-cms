9a9e20c2e23852ef6170633bf31e02a6959baa45
Lou Barnes <8981024@qq.com>
2023-03-12 14:21:32 +0800

    热门标签 换行

--- src/Section/SideTopTags/Template.php
+++ src/Section/SideTopTags/Template.php
@@ -35,8 +35,9 @@ class Template extends Section
         echo 'color: #fff;';
         echo 'background-color: var(--major-color);';
         echo 'padding: .1rem .5rem;';
-        echo 'margin-right: .25rem;';
         echo 'border-radius: .3rem;';
+        echo 'white-space:nowrap;';
+        echo 'word-break:keep-all;';
         echo '}';
 
         echo '#' . $this->id . ' .app-cms-side-top-tags .tag:hover {';
@@ -75,7 +76,7 @@ class Template extends Section
             <?php
             echo '<div class="be-mt-100 be-lh-175">';
             foreach ($topTags as $topTag) {
-                echo '<a class="tag" href="'. beUrl('Cms.Article.tag', ['tag' => $topTag]) .'">' . $topTag . '</a>';
+                echo '<a class="tag" href="'. beUrl('Cms.Article.tag', ['tag' => $topTag]) .'">' . $topTag . '</a> ';
             }
             echo '</div>';
 
